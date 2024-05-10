0e8663baa2ce4667e682cddcb3d2a30c0c5f065d
Ksenia Gerasimova <160503341+kotumba@users.noreply.github.com>
2024-05-10 20:18:16 +0400

    Добавлена форма обратной связи

---
+++ reviews-child/reviews-child.component.ts
@@ -0,0 +1,45 @@
+import { NgFor, NgIf } from '@angular/common';
+import { Component, Output, EventEmitter } from '@angular/core';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+
+
+export class User {
+  constructor(
+    public name: string,
+    public email: string,
+    public phone: string
+  ) {}
+}
+
+@Component({
+  selector: 'app-reviews-child',
+  standalone: true,
+  imports: [ReactiveFormsModule, NgIf, NgFor, FormsModule],
+  templateUrl: './reviews-child.component.html',
+  styleUrl: './reviews-child.component.css'
+})
+export class ReviewsChildComponent {
+  user: User[] = [];
+  user_name = '';
+  user_email = '';
+  user_phone = '';
+
+  @Output()
+  onClicked = new EventEmitter<string>();
+
+  addUser() {
+    this.console_log();
+    this.user.push(new User(this.user_name, this.user_email, this.user_phone));
+    this.user_name = '';
+  }
+
+  onNameChange(): void {
+    console.log('Изменение: ' + this.user_name);
+  }
+
+  console_log() {
+    this.onClicked.emit(
+      this.user_name + ' ' + this.user_email + '' + this.user_phone
+    );
+  }
+}
