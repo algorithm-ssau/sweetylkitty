ac14be5225aa6dd4496b07a6cc537724f76a8fd3
Ksenia Gerasimova <160503341+kotumba@users.noreply.github.com>
2024-05-10 18:23:13 +0400

    Adding new components

---
+++ configurator/configurator.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-configurator',
+  standalone: true,
+  imports: [],
+  templateUrl: './configurator.component.html',
+  styleUrl: './configurator.component.css'
+})
+export class ConfiguratorComponent {
+
+}
