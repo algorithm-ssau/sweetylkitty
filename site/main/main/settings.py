a8c1dca314bfe2528b4e4adb9795cd8a1297a59b
vlad3591 <vllad89879014105@yandex.ru>
2024-05-21 21:23:53 +0400

    Была добавлена таблица в базу данных для хранения информации о тортиках

--- site/main/main/settings.py
+++ site/main/main/settings.py
@@ -85,7 +85,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'sweetie_kitty',
         'USER': 'postgres',
-        'PASSWORD': 'aska1234',
+        'PASSWORD': '1234',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
--- site/main/postgresbd/models.py
+++ site/main/postgresbd/models.py
@@ -6,3 +6,7 @@ class Test(models.Model):
     title = models.CharField(max_length=70, blank=False, default='')
     description = models.CharField(max_length=200,blank=False, default='')
     
+class Cake (models.Model):
+    category = models.CharField(max_length=70, blank=False, default='')
+    title = models.CharField(max_length=70, blank=False, default='')
+    description = models.CharField(max_length=200,blank=False, default='')
