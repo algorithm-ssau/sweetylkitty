282d58fbbde642f8226b92d2a68c81e3c7afc30b
vlad3591 <vllad89879014105@yandex.ru>
2024-05-21 21:35:14 +0400

    Была добавлена таблица в базу данных для хранения информации об отзывах

--- site/main/postgresbd/models.py
+++ site/main/postgresbd/models.py
@@ -10,3 +10,8 @@ class Cake (models.Model):
     category = models.CharField(max_length=70, blank=False, default='')
     title = models.CharField(max_length=70, blank=False, default='')
     description = models.CharField(max_length=200,blank=False, default='')
+
+class Rewiew (models.Model):
+    email = models.CharField(max_length=70, blank=False, default='')
+    name = models.CharField(max_length=70, blank=False, default='')
+    description = models.CharField(max_length=200,blank=False, default='')
