9428a70cec2a6f00f99d2a4d18db75b095bba754
Ksenia Gerasimova <160503341+kotumba@users.noreply.github.com>
2024-05-22 01:16:38 +0400

    Улучшен каталог, добавлены категории

--- catalogue/catalogue.component.ts
+++ catalogue/catalogue.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { CategoryCatalogueComponent } from '../category-catalogue/category-catalogue.component';
+import { RouterLink, RouterModule, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-catalogue',
   standalone: true,
-  imports: [],
+  imports: [CategoryCatalogueComponent, RouterLink, RouterOutlet],
   templateUrl: './catalogue.component.html',
   styleUrl: './catalogue.component.css'
 })
