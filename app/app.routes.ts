823dcf3fb5e427df9ca16810484a0bb0ea047b00
vlad3591 <vllad89879014105@yandex.ru>
2024-06-01 23:51:54 +0400

    Удалены лишние файлы, подчищена структура проекта

--- app/app.routes.ts
+++
@@ -1,7 +0,0 @@
-import { Routes } from '@angular/router';
-import { MainMenuComponent } from './main-menu/main-menu.component';
-
-export const routes: Routes = [
-    { path: '', redirectTo: '/main-menu', pathMatch: 'full'},
-    {path: 'main-menu', component: MainMenuComponent}
-];
